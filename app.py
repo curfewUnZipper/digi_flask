cf26a7d8516d59b361359830e0357fe2945caacb
curfewUnzipper <cryingpushpa@gmail.com>
2026-05-08 06:30:45 +0530

    extra line removed

--- app.py
+++ app.py
@@ -18,8 +18,6 @@ import onnxruntime as ort
 # LOAD ENV VARIABLES
 # =========================================================
 
-load_dotenv()
-
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 print(SUPABASE_URL)
